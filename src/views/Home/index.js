2465670401d3a8341311f8aca6f63d66f2936d4b
Andrew Ton <aton4@uci.edu>
2021-09-26 21:02:09 -0700

    feat(home): create about section (#13)
    * feat(home): added about section
    
    * fix(about): change padding

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import About from '../About'
 import Contact from '../Contact'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#1a202c',
+    color: '#cbd5e0',
+  },
+}))
 
 function Home() {
+  const classes = useStyles()
+
   return (
-    <home>
+    <home className={classes.root}>
       <About />
       <Contact />
     </home>
