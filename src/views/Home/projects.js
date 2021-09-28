bed3125094010c38961ef59fea1ba17c29933979
Andrew Ton <aton4@uci.edu>
2021-09-28 00:31:49 -0700

    chore: post tempory work

--- src/views/Home/projects.js
+++ src/views/Home/projects.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@mui/styles'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
 import { Code } from '@mui/icons-material'
 import { projects } from './projectsData'
+import ArdentHQ from '../../images/ArdentHQ.png'
 
 const useStyles = makeStyles((theme) => ({
   section: {
@@ -26,18 +29,20 @@ const useStyles = makeStyles((theme) => ({
   },
   introductionTitle: {
     color: 'white',
-    fontSize: '1.875rem',
+    fontSize: '2rem',
     fontWeight: 500,
     marginBottom: '1rem',
   },
   introductionDescription: {
+    margin: '30px 0px 100px 0px',
     lineHeight: 1.625,
-    fontSize: '1rem',
+    fontSize: '1.5rem',
   },
   projectsContainer: {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     width: '100%',
+    height: '100%',
     color: '#444',
   },
   singleProjectContainer: {
@@ -57,6 +62,10 @@ const useStyles = makeStyles((theme) => ({
   },
   projectDescription: {
     textAlign: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    justifyItems: 'center',
     color: '#cbd5e0',
     fontSize: '1.5rem',
     lineHeight: 1.625,
@@ -64,6 +73,9 @@ const useStyles = makeStyles((theme) => ({
   viewProjects: {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
+    gridTemplateRows: '500px 500px',
+    gridGap: '30px',
+    margin: '0px 100px 0px 0px',
     alignItems: 'center',
     alignContent: 'center',
     justifyContent: 'center',
@@ -72,7 +84,49 @@ const useStyles = makeStyles((theme) => ({
   link: {
     display: 'flex',
   },
+  item: {
+    justifyContent: 'center',
+    display: 'flex',
+    width: '100%',
+    '&:hover': {
+      '&>a': {
+        color: 'green',
+      },
+    },
+  },
+  item2: {
+    margin: '0px 20px 0px 20px',
+    height: '500px',
+    '&:hover': {
+      '&>a': {
+        color: 'green',
+      },
+    },
+  },
 }))
+
+// singleProjectDetails: {
+//   display: 'flex',
+//   flexDirection: 'column',
+//   alignContent: 'center',
+//   justifyContent: 'center',
+// },
+
+const styles = {
+  paperContainer: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    textAlign: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    opacity: 0.5,
+    backgroundColor: 'transparent',
+    '&:hover': { transform: 'scale3d(1.05, 1.05, 1)', opacity: 1 },
+  },
+}
 
 function Projects() {
   const classes = useStyles()
@@ -85,25 +139,79 @@ function Projects() {
           <Code className={classes.codeIcon} sx={{ fontSize: 100 }} />
           <text className={classes.introductionTitle}>Apps I've Built</text>
           <text className={classes.introductionDescription}>
-            These are the various web applications that I've created so far mainly using React and Node.js
+            These are the various web applications that I've created so far, mainly using React and Node.js
           </text>
         </div>
-        <div className={classes.projectsContainer}>
-          <text className={classes.projectDescription}>{projectDescription}</text>
-          <div className={classes.viewProjects}>
-            {projects.map((project) => (
-              <div className={classes.singleProjectContainer} onClick={() => setProjectDescription(project.description)}>
-                <div className={classes.singleProjectDetails}>
-                  <text>{project.title}</text>
-                  <a href={project.link}>{project.linkType === 'GitHub' ? 'GitHub Link' : 'Website Link'}</a>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <Grid
+          container
+          xs={12}
+          className={classes.projectsContainer}
+          style={{
+            fontFamily: 'Old Newspaper',
+            marginLeft: '4px',
+            marginRight: '4px',
+          }}
+        >
+          <Grid item xs={5} className={classes.item}>
+            <text className={classes.projectDescription}>{projectDescription}</text>
+          </Grid>
+          <Grid item xs={6} className={classes.item}>
+            <Grid
+              container
+              spacing={4}
+              rowSpacing={5}
+              columnSpacing={{ xs: 15, sm: 35, md: 5 }}
+              style={{
+                fontFamily: 'Old Newspaper',
+                marginLeft: '4px',
+                marginRight: '4px',
+              }}
+            >
+              {projects.map((project) => (
+                <Grid item xs={6} className={classes.item2} style={{ backgroundImage: `url(${project.image})` }} />
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     </section>
   )
 }
 
+//     backgroundImage: `url(${ArdentHQ})`,
+{
+  /* <div className={classes.projectsContainer}>
+  <text className={classes.projectDescription}>{projectDescription}</text>
+  <div className={classes.viewProjects}>
+    {projects.map((project) => (
+      <Card
+        style={{ ...styles.paperContainer, backgroundImage: `url(${project.image})` }}
+        onClick={() => setProjectDescription(project.description)}
+      >
+        <div className={classes.singleProjectDetails}>
+          <text>{project.title}</text>
+          <a href={project.link}>{project.linkType === 'GitHub' ? 'GitHub Link' : 'Website Link'}</a>
+        </div>
+      </Card>
+    ))}
+  </div>
+</div> */
+}
+// https://stackoverflow.com/questions/64721468/having-trouble-creating-onhover-on-grid-item-component-materialui
+// https://stackoverflow.com/questions/63539865/spacing-in-material-ui-grid-not-working-as-expected
 export default Projects
+
+{/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+  <Grid item xs={6}>
+    aqwe
+  </Grid>
+  <Grid item xs={6}>
+    4535
+  </Grid>
+  <Grid item xs={6}>
+    sada
+  </Grid>
+  <Grid item xs={6}>
+    bvb
+  </Grid>
+</Grid> */}
--- src/views/Home/projectsData.js
+++ src/views/Home/projectsData.js
@@ -1,11 +1,13 @@
-import portfolioPhoto from '../../images/portfolioPhoto.jpg'
+import PortfolioSite from '../../images/PortfolioSite.png'
+import ExerTracker from '../../images/ExerTracker.png'
+import ArdentHQ from '../../images/ArdentHQ.png'
 
 export const projects = [
   {
     title: 'Portfolio Website',
     subtitle: 'React and Node.js',
     description: 'The website you are seeing right now is made with React and Node.js.',
-    image: './Portfolio_Website.png',
+    image: PortfolioSite,
     link: 'https://github.com/aton4/portfolio-website',
     linkType: 'GitHub',
   },
@@ -14,7 +16,7 @@ export const projects = [
     subtitle: 'MERN Stack Website',
     description:
       'A basic web app with MongoDB as the database, Express as the server, React as the tool to create the web pages, and Node.js as the JavaScript runtime environment. I created this project to teach some friends on how the backend and frontend of web apps are set up.',
-    image: './MERN_Website.png',
+    image: ExerTracker,
     link: 'https://github.com/aton4/MERN-Stack-Website',
     linkType: 'GitHub',
   },
@@ -23,7 +25,7 @@ export const projects = [
     subtitle: 'React and GraphQL',
     description:
       'Developed the web application for office employees to interact with classroom and student tables in our RethinkDB database. One of my main contributions is creating the login page.',
-    image: './Ardent_Office.png',
+    image: ArdentHQ,
     link: 'https://hq.ardentlabs.io/',
     linkType: 'Website',
   },
