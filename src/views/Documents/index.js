c12ab63b499a26fe723526fe268bc44410117914
Andrew Ton <aton4@uci.edu>
2021-10-04 19:07:43 -0700

    feat(documents): create iframe displaying resume pdf from google drive

--- src/views/Documents/index.js
+++ src/views/Documents/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function Documents() {
-  return <p>This is the Documents page</p>
+  return (
+    <iframe
+      title="My Computer Science Resume"
+      src="https://drive.google.com/file/d/1JZQ1DMF6Xv6ZkpSTFX8UVuJr5Jg84ylY/preview"
+      type="application/pdf"
+      width="100%"
+      height="95%"
+      autoplay="true"
+    />
+  )
 }
 
 export default Documents
